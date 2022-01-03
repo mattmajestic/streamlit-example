2ebdba77cdaac767dc9bac62b553fe1f996e39f8
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2022-01-03 12:48:46 -0500

    ui edit

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,12 +5,11 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit!
+# Cointel Streamlit App!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+You can see we are working in our `/streamlit_app.py` as a template of making a crypto currency dashboard :heart:
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+Checkout our [Cointel Finance Github](https://github.com/cointelfinance) & check out our partner (https://docs.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
